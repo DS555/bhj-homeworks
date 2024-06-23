4f6da805b31b86137ca238eddbeff28e9c1e9229
DS555 <daniil-sizov@yandex.ru>
2024-06-23 20:29:01 +0300

    first commit countdown

---
+++ js-features/countdown/task.js
@@ -0,0 +1,12 @@
+console.log('is worked');
+const timer = document.getElementById('timer');
+const mainCounter = function(){
+    let counter = +timer.textContent;
+    counter --;
+    timer.textContent = counter;
+if (timer.textContent === "0") {
+    alert('Вы победили в конкурсе!');
+    clearInterval(intervalID);
+}
+}
+let intervalID = setInterval(mainCounter, 1000);
